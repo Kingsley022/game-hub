e231331a37c5caf5d598a17be3855b6399dca79f
Okereke Kingsley <kingsleyokereke911@gmail.com>
2023-06-14 11:24:43 +0100

    optimised images

--- src/components/gameCard.tsx
+++ src/components/gameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 import { Game } from './../hooks/useGame';
 import PlatFormIconList from './platformIconList';
 import CriticScore from './criticScore';
+import getCroppedImageUrl from '../services/getcroppedImageUrl';
 
 
 interface Props{
@@ -12,7 +13,7 @@ const GameCard = ({game}:Props) => {
 
     return (
         <Card borderRadius={12} overflow='hidden'>
-            <Image src={game.background_image}/>
+            <Image src={getCroppedImageUrl(game.background_image)}/>
             <CardBody>
                 <Heading fontSize='1xl'>{game.name}</Heading>
                 <HStack justifyContent='space-between'>
